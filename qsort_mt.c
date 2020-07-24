943fc8741e9c9bf0ab15edf034dfd1ea2d4e73ba
blazer <blazerx@gmail.com>
2020-07-25 05:05:13 +1000

    Update qsort_mt.c

--- qsort_mt.c
+++ qsort_mt.c
@@ -420,7 +420,7 @@ top:
 			     pl > (char *)a && CMP(thunk, pl - es, pl) > 0;
 			     pl -= es) {
 				swap(pl, pl - es);
-				if (++swap_cnt > r) goto nevermind;
+				//if (++swap_cnt > r) goto nevermind;
 			}
 		return;
 	}
