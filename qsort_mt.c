2269db681307f44fe01bf31194c0d313d1f6a62b
blazer <blazerx@gmail.com>
2020-07-26 04:28:08 +1000

    Update qsort_mt.c

--- qsort_mt.c
+++ qsort_mt.c
@@ -359,7 +359,7 @@ top:
 			for (pl = pm;
 			     pl > (char *)a && CMP(thunk, pl - es, pl) > 0;
 			     pl -= es)
-				//swap(pl, pl - es);
+				swap(pl, pl - es);
 		return;
 	}
 	pm = (char *)a + (n / 2) * es;
@@ -405,8 +405,8 @@ top:
 		for (pm = (char *)a + es; pm < (char *)a + n * es; pm += es)
 			for (pl = pm;pl > (char *)a && CMP(thunk, pl - es, pl) > 0; pl -= es) {
 
-				swap(pl, pl - es);
-				puts("here1");
+				//swap(pl, pl - es);
+				//puts("here1");
 			}
 		return;
 	}
