e0aec387dd981f7821d03bf2696092d9ac4a76b1
blazer <blazerx@gmail.com>
2020-07-26 14:49:22 +1000

    Update qsort_mt.c

--- qsort_mt.c
+++ qsort_mt.c
@@ -367,7 +367,7 @@ top:
 	swap(a, pm);
 	pa = pb = (char *)a + es;
 	pc = pd = (char *)a + (n - 1) * es;
-	printf("Preloop %zu %zu %zu %zu \n", pa, pb, pc, pd);
+	printf("Preloop %c %c %c %c \n", pa, pb, pc, pd);
 
 	for (;;) {
 		while (pb <= pc && (r = CMP(thunk, pb, a)) <= 0) {
